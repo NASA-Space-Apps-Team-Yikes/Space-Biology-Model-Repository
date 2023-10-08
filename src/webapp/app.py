2e302db626410a875069890e5be2f41db79634f1
ryanmio <ryan@mioduski.us>
2023-10-08 15:26:52 -0400

    web app database connection
    added Doug's connection script into the routing
    
    Co-Authored-By: Doug Puccetti <dougpuccetti@gmail.com>

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -1,5 +1,42 @@
+import os
+import psycopg2
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+def connect_to_database():
+    conn = None
+    cursor = None
+    try:
+        conn = psycopg2.connect(host=os.getenv("DB_HOST"),
+                                port=os.getenv("DB_PORT"),
+                                database=os.getenv("DB_NAME"),
+                                user=os.getenv("DB_USER"),
+                                password=os.getenv("DB_PASSWORD"))
+        cursor = conn.cursor()
+        print("Connected")
+    except Exception as e:
+        print("Connection failed: {}".format(e))
+        print("Exception message: {}".format(e))
+    return conn, cursor
+
+@app.route('/database')
+def database():
+    conn, cursor = connect_to_database()
+    if cursor is not None:
+        try:
+            cursor.execute("SELECT * FROM actual_table_name WHERE id = 'ENSMUSG00000000031'")
+            rows = cursor.fetchall()
+        except Exception as e:
+            print("Failed to fetch data: {}".format(e))
+            rows = []
+        finally:
+            cursor.close()
+            conn.close()
+            print("Connection closed")
+        return str(rows)
+    else:
+        return "Failed to connect to the database"
 
 @app.route('/')
 def home():
