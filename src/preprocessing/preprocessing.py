a6cd490bafbcca87fa0a4d9258ddc5f8b6ba5a8e
ryanmio <ryan@mioduski.us>
2023-10-08 14:26:59 -0400

    Updated preprocessing.py
    Instead of removing rows with any zeros, the script will now replace 0s with NaN to ignore them in the averages
    
    Co-Authored-By: Doug Puccetti <dougpuccetti@gmail.com>

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -4,15 +4,15 @@ import numpy as np
 # Read the data
 df = pd.read_csv('Mouse_data.csv')
 
-# Remove rows with 0s
-df = df.loc[(df!=0).all(axis=1)]
+# Replace 0s with NaN
+df.replace(0, np.nan, inplace=True)
 
 # Split the column names to get the mouse type, test type, and mouse ID
 df.columns = df.columns.str.split('_', expand=True)
 
 # Define a function to create a new column name based on the mouse type and test type
 def create_new_column_name(col):
-    return f"{col[1]}_{col[2]}_{col[3]}_{col[4]}"
+    return f"{col[1]}_{col[3]}_{col[4]}".lower().replace('-', '_')
 
 # Apply the function to each column
 df.columns = df.columns.map(create_new_column_name)
@@ -20,17 +20,17 @@ df.columns = df.columns.map(create_new_column_name)
 # Exclude the protein column
 df_numeric = df.drop(columns=[df.columns[0]])
 
-# Average each column by mouse type and test type
+# Average each column by mouse type and test type, ignoring NaN values
 df_avg = df_numeric.groupby(by=df_numeric.columns, axis=1).mean()
 
 # Add the protein column back to the DataFrame
 df_final = pd.concat([df[df.columns[0]], df_avg], axis=1)
 
 # Rename the columns to indicate that they are averages
-df_final.columns = df_final.columns + '_AVG'
+df_final.columns = df_final.columns + '_avg'
 
 # Rename the protein column
-df_final.rename(columns={df_final.columns[0]: 'Protein_ID'}, inplace=True)
+df_final.rename(columns={df_final.columns[0]: 'ensembl_id'}, inplace=True)
 
 # Write the processed data to a new CSV file
 df_final.to_csv('processed_data.csv', index=False)
