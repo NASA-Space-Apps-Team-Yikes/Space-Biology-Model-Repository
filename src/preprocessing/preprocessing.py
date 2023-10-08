0c80f0257e6b1966be9cec181026ba09bae65f9f
ryanmio <ryan@mioduski.us>
2023-10-08 12:34:23 -0400

    updated script Implement to fix mouse data preprocessing
    This commit introduces a script for preprocessing mouse data. The script reads a CSV file, removes rows with 0s, splits the column names to extract mouse type and test type, and calculates the average for each combination of mouse type and test type. The processed data, which includes a 'Protein_ID' column and average columns for each mouse type and test type, is written to a new CSV file named 'processed_data.csv'.
    
    Co-Authored-By: Doug Puccetti <dougpuccetti@gmail.com>

--- src/preprocessing/preprocessing.py
+++ src/preprocessing/preprocessing.py
@@ -7,27 +7,30 @@ df = pd.read_csv('Mouse_data.csv')
 # Remove rows with 0s
 df = df.loc[(df!=0).all(axis=1)]
 
-# Split the column names to get the type and mouse
+# Split the column names to get the mouse type, test type, and mouse ID
 df.columns = df.columns.str.split('_', expand=True)
 
-# Define a function to create a new column name based on the type, mouse, and other details
+# Define a function to create a new column name based on the mouse type and test type
 def create_new_column_name(col):
-    return f"{col[0]}_{col[1]}_{col[2]}_{col[3]}_{col[4]}"
+    return f"{col[1]}_{col[3]}"
 
 # Apply the function to each column
 df.columns = df.columns.map(create_new_column_name)
 
-# Exclude the mouse ID column
+# Exclude the protein column
 df_numeric = df.drop(columns=[df.columns[0]])
 
-# Average each column by type and mouse
-df_numeric = df_numeric.groupby(by=df_numeric.columns, axis=1).mean()
+# Average each column by mouse type and test type
+df_avg = df_numeric.groupby(by=df_numeric.columns, axis=1).mean()
 
-# Add the mouse ID column back to the DataFrame
-df_final = pd.concat([df[df.columns[0]], df_numeric], axis=1)
+# Add the protein column back to the DataFrame
+df_final = pd.concat([df[df.columns[0]], df_avg], axis=1)
 
-# Rename the mouse ID column
-df_final.rename(columns={df_final.columns[0]: 'mouse_id'}, inplace=True)
+# Rename the columns to indicate that they are averages
+df_final.columns = df_final.columns + '_AVG'
+
+# Rename the protein column
+df_final.rename(columns={df_final.columns[0]: 'Protein_ID'}, inplace=True)
 
 # Write the processed data to a new CSV file
 df_final.to_csv('processed_data.csv', index=False)
